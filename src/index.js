c67e949bbefa0c174a4aaae4e8d8c9085c91fb8c
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-05-18 12:04:32 -0700

    docs(api): construct framework for index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/* This is the entry point the jest-geojson package. Example usage in other projects:
+
+       import * as matchers from 'jest-extended'
+       expect.extend(matchers)
+
+    Add new exports to this file in alphabetical order by folder structure to
+    make it easier to find and maintain.
+*/
+
+// Bounding Boxes
+
+// Coordinates
+
+// Features
+
+// Geometries
+
+// Properties
+
+// Winding
