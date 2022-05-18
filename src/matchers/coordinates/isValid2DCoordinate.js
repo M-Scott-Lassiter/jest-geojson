527bbc463c64e30546ff8def60776a07165adc64
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-05-18 15:51:50 -0700

    feat(isValid2DCoordinate): add new matcher function
    This matcher verifies that a 2D GeoJSON coordinate is appropriately formatted and in range.
    
    Resolves: #1

---
+++ src/matchers/coordinates/isValid2DCoordinate.js
@@ -0,0 +1,71 @@
+/**
+ * A helper function used to verify a coordinate has appropriate longitude and latitude values.
+ *
+ * @memberof Helpers
+ * @private
+ * @ignore
+ * @param {GeoJSON-Coordinate} coordinate A WGS-84 array of [longitude, latitude]
+ * @returns {boolean} True if a valid 2D GeoJSON coordinate, false otherwise
+ */
+function valid2D(coordinate) {
+    if (!Array.isArray(coordinate) || coordinate.length !== 2) {
+        return false
+    }
+
+    if (typeof coordinate[0] !== 'number' || typeof coordinate[1] !== 'number') {
+        return false
+    }
+
+    // eslint-disable-next-line no-self-compare
+    if (coordinate[0] !== coordinate[0] || coordinate[1] !== coordinate[1]) {
+        // Accounts for NaN
+        return false
+    }
+
+    if (coordinate[0] < -180 || coordinate[0] > 180 || coordinate[1] < -90 || coordinate[1] > 90) {
+        return false
+    }
+    return true
+}
+
+/**
+ * Verifies a two element coordinate meets WGS-84 and GeoJSON validity requirements.
+ *
+ * @memberof Coordinates
+ * @param {number[]} coordinateArray A two element array of numbers in format [longitude, latitude].
+ * Longitude must be between -180 to 180.
+ * Latitude must be between -90 to 90.
+ * @returns {JestMatchingObject} Test passed or failed
+ * @example
+ * expect([22, 45.733]).isValid2DCoordinate()
+ * expect([180, 90]).isValid2DCoordinate()
+ * @example
+ * expect([22, 100.56]).not.isValid2DCoordinate()
+ * expect([22, 45.733, 0]).not.isValid2DCoordinate()
+ * expect([[22, 45.733, 0]]).not.isValid2DCoordinate()
+ * expect([[22, 45.733], [180, 90]]).not.isValid2DCoordinate()
+ */
+function isValid2DCoordinate(coordinateArray) {
+    const { printReceived, matcherHint } = this.utils
+    const passMessage =
+        // eslint-disable-next-line prefer-template
+        matcherHint('.not.isValid2dCoordinate', '[longitude, latitude]', '') +
+        '\n\n' +
+        `Expected input to not be a two element array with longitude between (-90 to 90) and latitude between (-180 to 180).\n\n` +
+        `Received:  ${printReceived(coordinateArray)}`
+
+    const failMessage =
+        // eslint-disable-next-line prefer-template
+        matcherHint('.isValid2dCoordinate', '[longitude, latitude]', '') +
+        '\n\n' +
+        `Expected a two element array with longitude between (-90 to 90) and latitude between (-180 to 180).\n\n` +
+        `Received:  ${printReceived(coordinateArray)}`
+
+    if (valid2D(coordinateArray)) {
+        return { pass: true, message: () => passMessage }
+    }
+    return { pass: false, message: () => failMessage }
+}
+
+exports.valid2D = valid2D
+exports.isValid2DCoordinate = isValid2DCoordinate
---
+++ tests/coordinates/isValid2DCoordinate.test.js
@@ -0,0 +1,171 @@
+const goodCoordinates = [
+    [0, 0],
+    [102.0, 0.5],
+    [172.0, -15],
+    [-10.9, 77],
+    [-152.0, -33.33333]
+]
+const goodBoundaryCoordinates = [
+    [180, 0],
+    [-180, 0],
+    [0, 90],
+    [0, -90],
+    [180, 90],
+    [180, -90],
+    [-180, 90],
+    [-180, -90]
+]
+const coordinatesOutOfRange = [
+    [0, 90.0000001],
+    [0, -90.0000001],
+    [0, 900000],
+    [0, -900000],
+    [180.0000001, 0],
+    [-180.0000001, 0],
+    [1800000, 0],
+    [-1800000, 0],
+    [181, 91],
+    [181, -91],
+    [-181, 91],
+    [-181, -91]
+]
+const invalidInputValues = [
+    undefined,
+    null,
+    true,
+    false,
+    200,
+    -200,
+    Infinity,
+    -Infinity,
+    NaN,
+    { coordinates: [0, 0] },
+    '',
+    'Random Coordinate',
+    '[0, 0]',
+    '[[0, 0], [0, 0]]'
+]
+
+describe('Passing Good Coordinates to Expect', () => {
+    test.each([...goodCoordinates])(
+        'Expect to pass with good coordinate: expect([%p, %p])',
+        (longitude, latitude) => {
+            expect([longitude, latitude]).isValid2DCoordinate()
+        }
+    )
+
+    test.each([...goodBoundaryCoordinates])(
+        'Expect to pass with good boundary coordinate: expect([%p, %p])',
+        (longitude, latitude) => {
+            expect([longitude, latitude]).isValid2DCoordinate()
+        }
+    )
+})
+
+describe('Error Snapshot Testing', () => {
+    test('.not.isValid2DCoordinate', () => {
+        expect(() => expect([0, 0]).not.isValid2DCoordinate()).toThrowErrorMatchingSnapshot()
+    })
+
+    test('.isValid2DCoordinate', () => {
+        expect(() => expect(false).isValid2DCoordinate()).toThrowErrorMatchingSnapshot()
+    })
+})
+
+describe('Passing Bad Inputs to Expect', () => {
+    test.each([...invalidInputValues])('Expect to fail with bad input: expect(%p)', (badInput) => {
+        expect(badInput).not.isValid2DCoordinate()
+    })
+})
+
+describe('Passing Incorrect Number of Arguments to Expect', () => {
+    test.each([[[]], [[20]], [[20, 30, 0]], [[20, 30, 0, 20, 30, 0, 20, 30, 0]]])(
+        'Expect to fail with bad input: expect(%p)',
+        (badInput) => {
+            expect(badInput).not.isValid2DCoordinate()
+        }
+    )
+})
+
+describe('Passing Coordinates out of Range to Expect', () => {
+    test.each([...coordinatesOutOfRange])(
+        'Expect to fail with out of range coordinate: expect([%p, %p])',
+        (longitude, latitude) => {
+            expect([longitude, latitude]).not.isValid2DCoordinate()
+        }
+    )
+})
+
+describe('Passing Bad Individual Coordinate Values to Expect', () => {
+    test.each([...invalidInputValues])(
+        'Expect to fail with bad longitude value: expect([%p, 0])',
+        (longitude) => {
+            expect([longitude, 0]).not.isValid2DCoordinate()
+        }
+    )
+
+    test.each([...invalidInputValues])(
+        'Expect to fail with bad latitude value: expect([0, %p])',
+        (latitude) => {
+            expect([0, latitude]).not.isValid2DCoordinate()
+        }
+    )
+
+    test.each([...invalidInputValues])('Expect to fail with bad values for both: %p', (input) => {
+        expect([input, input]).not.isValid2DCoordinate()
+    })
+})
+
+describe('Arrays Nested Too Deeply', () => {
+    test.each([
+        [
+            [
+                [
+                    [1, 1],
+                    [0, 0]
+                ]
+            ]
+        ],
+        [
+            [
+                [
+                    [10, 20],
+                    [2, 59]
+                ]
+            ]
+        ],
+        [
+            [
+                [
+                    [10, 20],
+                    [2, 90],
+                    [95, 5]
+                ]
+            ]
+        ],
+        [
+            [
+                [
+                    [
+                        [1, 1],
+                        [0, 0]
+                    ]
+                ]
+            ]
+        ],
+        [
+            [
+                [
+                    [
+                        [
+                            [2, 2],
+                            [3, 3]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ])('Expect to fail with bad input: expect(%p)', (badInput) => {
+        expect(badInput).not.isValid2DCoordinate()
+    })
+})
