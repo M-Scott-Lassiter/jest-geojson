ac05626a146717f0b7b64a7f34d5e269f593b652
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-05-23 08:18:58 -0700

    build(package): update the commitizen config for customized scopes
    Updates @m-scott-lassiter/semantic-release-github-npm-config to v2.0.0 which treats the `api` type
    as a patch release trigger. The commitizen config thus adds an `api` type as well as lays out the
    groundwork for more descriptive scopes.

--- .cz-config.js
+++ .cz-config.js
@@ -1,5 +1,43 @@
+// When adding new matchers, place them in alphabetical order inside the appropriate category
+
+const coordinateMatchers = [{ name: 'isValid2DCoordinate' }, { name: 'isValid3DCoordinate' }]
+
+const boundingBoxMatchers = []
+
+const geometryMatchers = []
+
+const geometryCollectionMatchers = []
+
+const featureMatchers = []
+
+const featureCollectionMatchers = []
+
+const functionalMatchers = []
+
+const allMatchers = [
+    ...coordinateMatchers,
+    ...boundingBoxMatchers,
+    ...geometryMatchers,
+    ...geometryCollectionMatchers,
+    ...featureMatchers,
+    ...featureCollectionMatchers,
+    ...functionalMatchers
+]
+
+const documentationScopes = [
+    { name: 'contributing' },
+    { name: 'code of conduct' },
+    { name: 'license' },
+    { name: 'readme' },
+    { name: 'security' }
+]
+
 module.exports = {
     types: [
+        {
+            value: 'api',
+            name: 'api:      Non-functional changes to code API documentation that help other developers understand how to use a tool or feature (i.e. intellisense)'
+        },
         {
             value: 'build',
             name: 'build:    Changes that affect the build system configuration, package scripts, or dev dependencies (i.e. adds/remove/modify/update)'
@@ -9,7 +47,7 @@ module.exports = {
             name: 'ci:       Changes to CI configuration files and scripts (e.g. release configs, YAML scripts)'
         },
         { value: 'docs', name: 'docs:     Documentation only changes' },
-        { value: 'feat', name: 'feat:     Adds or enhances a new matcher' },
+        { value: 'feat', name: 'feat:     Adds a new feature' },
         {
             value: 'fix',
             name: 'fix:      Fixes a bug in an existing feature. Also used for non-dev dependency updates.'
@@ -26,13 +64,7 @@ module.exports = {
         { value: 'test', name: 'test:     Add missing tests or correct existing tests' }
     ],
 
-    scopes: [
-        { name: 'api' },
-        { name: 'contributing' },
-        { name: 'license' },
-        { name: 'readme' },
-        { name: 'security' }
-    ],
+    scopes: [...allMatchers, ...documentationScopes],
 
     allowTicketNumber: false,
     isTicketNumberRequired: false,
@@ -40,16 +72,22 @@ module.exports = {
     ticketNumberRegExp: '\\d{1,5}',
 
     // it needs to match the value for field type. Eg.: 'fix'
-    /*
     scopeOverrides: {
-      fix: [
-        {name: 'merge'},
-        {name: 'style'},
-        {name: 'e2eTest'},
-        {name: 'unitTest'}
-      ]
+        api: allMatchers,
+        ci: [{ name: 'publish' }, { name: 'test' }, { name: 'hooks' }, { name: 'templates' }],
+        build: [
+            { name: 'dependencies' },
+            { name: 'devDependencies' },
+            { name: 'scripts' },
+            { name: 'package' }
+        ],
+        docs: documentationScopes,
+        fix: allMatchers,
+        perf: allMatchers,
+        refactor: allMatchers,
+        test: allMatchers
     },
-    */
+
     // override the messages, defaults are as follows
     messages: {
         type: "Select the type of change that you're committing:",
