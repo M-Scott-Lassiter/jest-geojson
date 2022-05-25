7e3e8ed9e8603d8295ee77f4391dd3fe4077af91
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-05-25 14:20:07 -0700

    api(isValidCoordinate): update the error and parameter descriptions

--- src/core/coordinates/validCoordinate.js
+++ src/core/coordinates/validCoordinate.js
@@ -8,8 +8,7 @@ const { valid3DCoordinate } = require('./valid3DCoordinate')
  * @see https://github.com/M-Scott-Lassiter/jest-geojson/issues/4
  * @param {GeoJSON2DCoordinate|GeoJSON3DCoordinate} coordinate A WGS-84 array of [longitude, latitude] or [longitude, latitude, alititude]
  * @returns {boolean} True if a valid 3D GeoJSON coordinate. If invalid, it will throw an error.
-//  * @throws {Error} Input must be an array of only three elments
-//  * @throws {Error} Altitude value must be numeric
+ * @throws {Error} Input must be an array of only two or three elments
  */
 function validCoordinate(coordinate) {
     if (!Array.isArray(coordinate) || coordinate.length < 2 || coordinate.length > 3) {
